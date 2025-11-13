7326729d0d76696f38bee70854b9cf0b7b2fd29e
yaboi14 <mateotmedak08@gmail.com>
2025-11-13 09:39:54 -0800

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String [] args){
-        Dog dog1 = new Dog("Playful", "Buddy", "Brown", "Labrador", 3, "Medium");
-        Dog dog2 = new Dog("Calm", "Max", "Black", "German Shepherd", 5, "Large");
-
-        System.out.println("Dog 1: " + dog1.getName() + ", Personality: " + dog1.getPersonality());
-        System.out.println("Dog 2: " + dog2.getName() + ", Personality: " + dog2.getPersonality());
-    } 
-}
